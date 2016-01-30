c79e8422e8d770fd631372cc00338b23e5b1a1c6
Erica Andrew <erica.nicole.andrew@gmail.com>
2016-01-30 13:15:37 -0700

    Completed callbackPractic and Jasmine debug

--- callbackPractice.js
+++ callbackPractice.js
@@ -26,10 +26,13 @@ and what you should write is the sayHi function that makes the code above work,
 
   //Code Here for first
   
+function first(arr, cb) {
+    cb(arr[0]);
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
-  console.log('The first name in names is ' + firstName)
+  console.log('The first name in names is ' + firstName);
 });
 
 
@@ -40,6 +43,10 @@ first(names, function(firstName){
 
 
   //Code Here for last
+  
+function last(arr, cb) {
+    cb(arr[arr.length - 1]);
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 last(names, function(lastName){
@@ -58,22 +65,41 @@ last(names, function(lastName){
 
 
   //Code Here for multiply
+  
+function multiply(x, y, cb) {
+    cb(x * y);
+}
 
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
-})
-
-
-
+});
 
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
-
-
-
   //Code Here for contains
+  
+//function contains(arr, str, cb) {  //works but does not meet spec
+//    var check;
+//    arr.forEach(function(e){
+//        if(e === str) {
+//            check = true;
+//        }
+//    });
+//    cb(check);
+//}
+
+function contains(arr, str, cb) {
+    arr.forEach(function(e){
+        if(e === str) {
+            cb(true);
+        } else {
+            cb(false);
+        }
+    });
+
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 contains(names, 'Colt', function(result){
@@ -90,10 +116,22 @@ contains(names, 'Colt', function(result){
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
-
+//sortArr = ['Blaine', 'Cahlan', 'Cahlan', 'Colt', 'Ryan', 'Tyler', 'Tyler']
 
 
     //Code Here for uniq
+    
+function uniq(arr, cb) {
+    var uniqObj = {};
+    var newArr = [];
+    arr.forEach(function(e){  // now I have an object call uniqObj with each name as a key, keys cannot duplicate, so you only get one each
+        uniqObj[e] = null;
+    });
+    for (var key in uniqObj) {
+        newArr.push(key);
+    }
+    cb(newArr);
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 uniq(names, function(uniqArr){
@@ -108,12 +146,16 @@ uniq(names, function(uniqArr){
 
 
 
-
-    //Code Here for each
+function each(arr, cb) {  //cb will need to provide indice and item
+    arr.forEach(function(item, i){
+        cb(item, i);
+    });
+    
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
-  console.log('The item in the ' + indice + ' position is ' + item)
+  console.log('The item in the ' + indice + ' position is ' + item);
 });
 
 
@@ -126,7 +168,15 @@ each(names, function(item, indice){
 
 
 
- //code here for getUserById
+function getUserById(arr, str, cb) {  //will need to return an object
+    var correctObj = {};
+    arr.forEach(function(item){
+        if(item.id === str) {
+            correctObj = item;
+        }
+    });
+    cb(correctObj);
+};
 
 var users = [
   {
@@ -146,7 +196,7 @@ var users = [
     email: 'ryan@gmail.com',
     name: 'Ryan',
     address: '192 East 32 North'
-  },
+  }
 ];
 
 getUserById(users, '16t', function(user){
